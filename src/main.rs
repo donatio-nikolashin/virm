8fb21c8f2308e793ecb16cb60e2c7455571f2948
Nikolashin <donatio.nikolashin@south.rt.ru>
2022-05-20 01:24:18 +0300

    Add crossterm dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{Event, KeyCode, KeyEvent}; 
+use crossterm::{event, terminal};
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not turn off Raw mode");
+    }
+}
+
+fn main() {	
+    let _clean_up = CleanUp;
+    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
+    loop {
+        if let Event::Key(event) = event::read().expect("Failed to read line") {
+            match event {
+                KeyEvent {
+                    code: KeyCode::Char('q'),
+                    modifiers: event::KeyModifiers::NONE,
+                } => break,
+                _ => {
+                    //todo
+                }
+            }
+            println!("{:?}\r", event);
+        };
+    }
+    /* let mut buf = [0; 1];
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
+        let character = buf[0] as char;
+        if character.is_control() {
+            println!("{}\r", character as u8)
+        } else {
+            println!("{}\r", character)
+        }
+    }
+    panic!(); */
 }
