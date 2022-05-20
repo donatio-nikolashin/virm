897cf89d71790cfb9d19017eb625ad5556d6f480
Nikolashin <donatio.nikolashin@south.rt.ru>
2022-05-21 01:03:17 +0300

    Add cursor & line marks

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,147 @@
-use crossterm::event::{Event, KeyCode, KeyEvent}; 
-use crossterm::{event, terminal};
+use std::io;
+use std::io::{stdout, Write};
+use std::time::Duration;
+
+use crossterm::{cursor, execute, queue, terminal};
+use crossterm::event::*;
+use crossterm::terminal::ClearType;
 
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Could not turn off Raw mode");
+        Output::clear_screen().expect("Error");
     }
 }
 
-fn main() {	
-    let _clean_up = CleanUp;
-    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
-    loop {
-        if let Event::Key(event) = event::read().expect("Failed to read line") {
-            match event {
-                KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: event::KeyModifiers::NONE,
-                } => break,
-                _ => {
-                    //todo
-                }
-            }
-            println!("{:?}\r", event);
-        };
-    }
-    /* let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
-        let character = buf[0] as char;
-        if character.is_control() {
-            println!("{}\r", character as u8)
-        } else {
-            println!("{}\r", character)
+struct EditorContents {
+    content: String,
+}
+
+impl EditorContents {
+    fn new() -> Self {
+        Self {
+            content: String::new(),
         }
     }
-    panic!(); */
+
+    fn push(&mut self, ch: char) {
+        self.content.push(ch)
+    }
+
+    fn push_str(&mut self, string: &str) {
+        self.content.push_str(string)
+    }
+}
+
+impl Write for EditorContents {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(s.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into()),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
+    }
+}
+
+struct Output {
+    win_size: (usize, usize),
+    editor_contents: EditorContents,
+}
+
+impl Output {
+    fn new() -> Self {
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
+        Self {
+            win_size,
+            editor_contents: EditorContents::new(),
+        }
+    }
+
+    fn draw_rows(&mut self) {
+        let screen_rows = self.win_size.1;
+        for i in 0..screen_rows {
+            self.editor_contents.push('~');
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            ).unwrap();
+            if i < screen_rows - 1 {
+                self.editor_contents.push_str("\r\n");
+            }
+        }
+    }
+
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All))?;
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
+        self.draw_rows();
+        queue!(self.editor_contents, cursor::MoveTo(0, 0), cursor::Show)?;
+        self.editor_contents.flush()
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = read()? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+    output: Output,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader, output: Output::new() }
+    }
+
+    fn process_keypress(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL,
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&mut self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
+        self.process_keypress()
+    }
+}
+
+fn main() -> crossterm::Result<()> {
+    let _clean_up = CleanUp;
+    terminal::enable_raw_mode()?;
+    let mut editor = Editor::new();
+    while editor.run()? {}
+    Ok(())
 }
